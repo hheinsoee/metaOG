fe077a2e3f18324d5009e649d18f6f0524c867b7
Hein Soe <35089574+hheinsoee@users.noreply.github.com>
2024-09-24 22:47:26 +0630

    update test script

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,12 @@
 import { metaOG } from ".";
 
-
 describe("metaOG", () => {
   it("open graph phase", async () => {
     const url = "https://www.bbc.com/news/live/c5y8xkejjn3t";
     const ogData = await metaOG(url);
 
     expect(ogData).toHaveProperty("title");
+    expect(ogData).toHaveProperty("description");
+    expect(ogData).toHaveProperty("og:image");
   });
 });
